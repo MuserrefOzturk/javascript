90db91555aadfc381b0df942b6904d584fe5484d
belindaxyz <118962940+belindaxyz@users.noreply.github.com>
2023-10-19 12:24:37 +0300

    mysite

---
+++ case2/js/i1.js
@@ -0,0 +1,9 @@
+function bir(){
+    document.getElementById("x").src="img/1.jpg";
+}
+function iki(){
+    document.getElementById("x").src="img/2.jpg";
+}
+function uc(){
+    document.getElementById("x").src="img/3.jpg";
+}
