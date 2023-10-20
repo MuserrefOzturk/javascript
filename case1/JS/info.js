79f57a24a383677df8f7fee1704494f287670b06
belindaxyz <118962940+belindaxyz@users.noreply.github.com>
2023-10-20 10:39:24 +0300

    bootsrap

---
+++ case1/JS/info.js
@@ -0,0 +1,11 @@
+let a="Hello World!"
+document.getElementById("demo").innerHTML=a;
+function myfunction(){
+    alert("Welcome to Javascript World...");
+}
+function myfunction2(){
+    console.log("Hello again!");
+}
+function myfunction3(){
+    document.getElementById("demo").innerHTML="Hello There";
+}
